185ccdd8692a7610321e5e5e8c52412149a2ea13
dedaje <mhel38989@edu.ucl.dk>
2022-09-09 11:05:56 +0200

    alert button change

---
+++ LevSundt-Semesterprojekt/Pages/MessegeAlert/buttonobjecct.cs
@@ -0,0 +1,13 @@
+﻿namespace bmi_razor.Pages.bmi
+{
+    public class buttonobjecct
+    {
+        public string AlertButtonhidden { get; set; } = "hidden";
+        public string? AlertArticalCss { get; set; }
+     
+  
+        public string? AlertButtonCss { get; set; }
+        public string? AlertStrongText { get; set; }
+        public string? AlertPText { get; set; }
+    }
+}
